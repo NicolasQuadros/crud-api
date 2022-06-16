49947d26b9d75052ca4f62cfb27b605783a6a36a
roehrigjunior <93654557+roehrigjunior@users.noreply.github.com>
2022-06-16 15:22:44 -0300

    Update contract.controller.js

--- server/controllers/contract.controller.js
+++ server/controllers/contract.controller.js
@@ -2,6 +2,24 @@ const db = require("../models/sequelize");
 const Contract = db.contract;
 const Op = db.Sequelize.Op;
 
-/**
- * Add controllers here
- */
+// Find one contract
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    console.log(`\n[REQUEST: FIND CONTRACT WITH ID=${id}]`);
+    Contract.findByPk(id)
+    .then(data => {
+        if (data) {
+            res.send(data);
+        } else {
+            res.status(404).send({
+                message: `Cannot find contract with id=${id}.`
+        });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || `An error occurred while finding a contract with id=${id}.`
+        });
+    });
+  };
