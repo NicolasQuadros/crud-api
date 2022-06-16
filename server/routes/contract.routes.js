22fac3e4d31a27f63930d498147a14bd70bfe2e8
roehrigjunior <93654557+roehrigjunior@users.noreply.github.com>
2022-06-16 15:17:45 -0300

    Update contract.routes.js

--- server/routes/contract.routes.js
+++ server/routes/contract.routes.js
@@ -2,9 +2,8 @@ module.exports = app => {
     const contract = require("../controllers/contract.controller.js");
     var router = require("express").Router();
     
-    /**
-     * Add routes here
-     */
+   // Update with id
+    router.put("/:id", contract.update);
 
     app.use('/contracts', router);
   };
