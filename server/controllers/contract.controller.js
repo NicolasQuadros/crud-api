be21d96c790ec7c57f91ea7321d5fdeeb977be0f
roehrigjunior <93654557+roehrigjunior@users.noreply.github.com>
2022-06-16 15:22:17 -0300

    Update contract.controller.js

--- server/controllers/contract.controller.js
+++ server/controllers/contract.controller.js
@@ -2,6 +2,28 @@ const db = require("../models/sequelize");
 const Contract = db.contract;
 const Op = db.Sequelize.Op;
 
-/**
- * Add controllers here
- */
+// Update a contract
+exports.update = (req, res) => {
+    const id = req.params.id;
+    console.log(`\n[REQUEST: UPDATE CONTRACT WITH ID=${id}]`);
+    Contract.update(req.body, {
+        where: { id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "The contract was updated successfully."
+            });
+        } else {
+            res.send({
+                message: `Cannot update contract with id=${id}. Maybe Contract was not found or req.body is empty.`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || `An error occurred while updating a contract with id=${id}.`
+        });
+    });
+};
